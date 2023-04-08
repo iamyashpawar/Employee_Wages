8a95ae2b3a79fe183c1c93b74fd3bb8952a78392
Yash <imyashpawar@gmail.com>
2023-04-08 11:03:57 +0530

    Message display

--- src/com/bridgelabz/Employee_Wages.java
+++ src/com/bridgelabz/Employee_Wages.java
@@ -1,10 +1,14 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Employee_Wages
 {
     public static void main(String[] args)
     {
-         
+        System.out.println("Welcome to Employee Wage");
+
+
 
     }
 }
