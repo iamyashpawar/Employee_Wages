6108116dce34db6fa536d2780fc50bb64d8e0609
Yash <imyashpawar@gmail.com>
2023-04-08 12:09:02 +0530

    Added: uc4_Switch Case

--- src/com/bridgelabz/Employee_Wages.java
+++ src/com/bridgelabz/Employee_Wages.java
@@ -11,29 +11,29 @@ public class Employee_Wages
     static int PART_TIME_HOURS = 4;
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Employee Wage");
+
 
         Random rd = new Random();
           int dailyWage=0;
         int emp = rd.nextInt(3);
-        if(emp == 1)
-        {
-            System.out.println("Employee is present");
-            dailyWage = WAGE_PER_HOUR * FULL_TIME_WAGE;
-            System.out.println("Your Daily Wage is : " + dailyWage);
+        System.out.println("**** "+emp+ " ****");
 
-        } else if (emp == 2)
+        switch (emp)
         {
-            System.out.println("Employee worked part time");
-            dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
-            System.out.println("Your Half Day Wage is : " + dailyWage);
+            case 1 :
+                System.out.println("Employee Was Present Full Time");
+                dailyWage = WAGE_PER_HOUR * FULL_TIME_WAGE;
+                System.out.println("Your Daily wage is : " + dailyWage);
+                break;
+            case 2 :
+                System.out.println("Employee Was Present Half Day");
+                dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+                System.out.println("Your Half Day Wage is : " + dailyWage);
+                break;
+
+            default:
+                System.out.println("Employee Was Absent!!!");
         }
-        else
-        {
-            System.out.println("Employee is Absent!!");
-        }
-
-
 
     }
 }
