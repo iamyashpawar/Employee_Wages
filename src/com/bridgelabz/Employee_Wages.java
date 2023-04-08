bfb7262ef67be4eaa2e0fbe830608d65ac594b3d
Yash <imyashpawar@gmail.com>
2023-04-08 12:29:16 +0530

    Added: Montly payment

--- src/com/bridgelabz/Employee_Wages.java
+++ src/com/bridgelabz/Employee_Wages.java
@@ -8,6 +8,8 @@ public class Employee_Wages
     static int WAGE_PER_HOUR=20;
     static int FULL_TIME_WAGE = 8;
 
+    static int MONTHLY_TOTAL_DAYS = 20;
+
     static int PART_TIME_HOURS = 4;
     public static void main(String[] args)
     {
@@ -15,6 +17,7 @@ public class Employee_Wages
 
         Random rd = new Random();
           int dailyWage=0;
+          int monthlyPayment=0;
         int emp = rd.nextInt(3);
         System.out.println("**** "+emp+ " ****");
 
@@ -24,11 +27,15 @@ public class Employee_Wages
                 System.out.println("Employee Was Present Full Time");
                 dailyWage = WAGE_PER_HOUR * FULL_TIME_WAGE;
                 System.out.println("Your Daily wage is : " + dailyWage);
+                monthlyPayment = dailyWage * MONTHLY_TOTAL_DAYS;
+                System.out.println("Your Monthly Payment is : "+ monthlyPayment);
                 break;
             case 2 :
                 System.out.println("Employee Was Present Half Day");
                 dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
                 System.out.println("Your Half Day Wage is : " + dailyWage);
+                monthlyPayment = dailyWage * MONTHLY_TOTAL_DAYS;
+                System.out.println("Your Monthly payment(Part time) : " + monthlyPayment);
                 break;
 
             default:
